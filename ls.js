d12fe91e9d8048327b59e601637af004562c4fe1
WindexIM <voxel0@disroot.org>
2025-07-28 14:07:44 -0400

    js es Mierda de Huevones

--- ls.js
+++ ls.js
@@ -20,7 +20,7 @@ function updateLanguage(lang) {
   document.querySelectorAll("[data-i18n]").forEach(el => {
     const key = el.getAttribute("data-i18n");
     if (translations[lang] && translations[lang][key]) {
-      el.textContent = translations[lang][key];
+      el.innerHTML = translations[lang][key];
     }
   });
 }
